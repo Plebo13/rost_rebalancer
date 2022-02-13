ca9e1dfcbffab9e6461b73eb72359d8411aa1fbe
Lukas Brauckmann <lukas.brauckmann@gmail.com>
2022-02-13 11:16:48 +0100

    Establish connection to database

---
+++ src/asset.rs
@@ -0,0 +1,9 @@
+pub struct ETF {
+    isin: String,
+    name: String,
+}
+
+pub struct Coin {
+    id: String,
+    name: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use postgres::{Client, NoTls};
 fn main() {
-    println!("Hello, world!");
+    let mut client = Client::connect(
+        "postgresql://postgres@127.0.0.1:5432/rost_rebalancer",
+        NoTls,
+    )
+    .unwrap();
+
+    for row in client
+        .query("SELECT id, name, quantity FROM assets", &[])
+        .unwrap()
+    {
+        let id: &str = row.get(0);
+        let name: &str = row.get(1);
+        let quantity: f32 = row.get(2);
+
+        println!("Found asset: {} {} {:?}", id, name, quantity);
+    }
 }
